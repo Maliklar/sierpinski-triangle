1043f7f50938924f942648a82f74cdb3ad331470
Malik Elbadri <46916173+Maliklar@users.noreply.github.com>
2025-02-06 11:57:46 +0300

    Created React Project

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 const canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
+const content = document.getElementById("content");
 
-const rec = canvas.getBoundingClientRect();
-canvas.height = rec.height;
-canvas.width = rec.width;
-const { height, width } = canvas;
-const SIDES = 8;
-const context = canvas.getContext("2d");
+content.appendChild(canvas);
 
-// const triangle = drawTriangle();
-const initialPoint = drawInitialPoint();
-const polygon = generatePolygonCorners(SIDES);
-drawRandom();
+let SIDES = 5;
 
-function generatePolygonCorners(sides) {
-  if (sides < 3) return new Error("Sides must be at least 3");
-  const centerX = width / 2;
-  const centerY = height / 2;
-  const radius = Math.min(width, height) / 2;
-  const angleStep = (2 * Math.PI) / sides;
-  const points = Array.from({ length: sides }, (_, i) => {
-    const angle = i * angleStep;
-    return {
-      x: centerX + radius * Math.cos(angle),
-      y: centerY + radius * Math.sin(angle),
-    };
-  });
-  context.beginPath();
-  context.strokeStyle = "white";
-  context.moveTo(points[0].x, points[0].y);
-  console.log(points);
-  for (let i = 1; i < points.length; i++)
-    context.lineTo(points[i].x, points[i].y);
-  context.closePath();
-  context.stroke();
-  return points;
-}
+function start(SIDES) {
+  const rec = canvas.getBoundingClientRect();
+  canvas.height = rec.height;
+  canvas.width = rec.width;
+  const { height, width } = canvas;
+  const context = canvas.getContext("2d");
+  const initialPoint = drawInitialPoint();
 
-function drawRandom() {
+  const polygon = generatePolygonCorners(SIDES);
+  function generatePolygonCorners(sides) {
+    if (sides < 3) return new Error("Sides must be at least 3");
+    const centerX = width / 2;
+    const centerY = height / 2;
+    const radius = Math.min(width, height) / 2;
+    const angleStep = (2 * Math.PI) / sides;
+    const points = Array.from({ length: sides }, (_, i) => {
+      const angle = i * angleStep;
+      return {
+        x: centerX + radius * Math.cos(angle),
+        y: centerY + radius * Math.sin(angle),
+      };
+    });
+    context.beginPath();
+    context.strokeStyle = "white";
+    context.moveTo(points[0].x, points[0].y);
+    console.log(points);
+    for (let i = 1; i < points.length; i++)
+      context.lineTo(points[i].x, points[i].y);
+    context.closePath();
+    context.stroke();
+    return points;
+  }
+
   let lastVertex = initialPoint;
 
   let position = {};
 
-  setInterval(() => {
-    for (let i = 0; i < 100; i++) {
+  const interval = setInterval(() => {
+    for (let i = 0; i < 10; i++) {
       const corner = Math.floor(Math.random() * SIDES);
       const c = polygon.at(corner);
       position = halfDistance(lastVertex.x, lastVertex.y, c.x, c.y);
@@ -51,38 +51,48 @@ function drawRandom() {
       drawPoint(lastVertex.x, lastVertex.y, getRandomColor());
     }
   });
-}
 
-function* nextColor() {
-  let i = 50;
-  while (true) {
-    i++;
-    if (i > 256) i = 50;
-    yield i;
+  function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
   }
-}
-const next = nextColor();
 
-function getRandomColor() {
-  const r = next.next().value;
-  const g = next.next().value;
-  const b = next.next().value;
-  return `rgb(${r},${g},${b})`;
+  function halfDistance(x1, y1, x2, y2) {
+    return { x: (x2 + x1) / 2, y: (y2 + y1) / 2 };
+  }
+
+  function drawInitialPoint(color = "black") {
+    const initialPoint = { x: width / 2, y: height / 2 };
+    drawPoint(initialPoint.x, initialPoint.y, color);
+    return initialPoint;
+  }
+  function drawPoint(x, y, color = "black") {
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x, y, 1, 0, Math.PI * 2, true);
+    context.fill();
+    context.closePath();
+  }
+
+  return () => {
+    context.reset();
+    clearInterval(interval);
+  };
 }
 
-function halfDistance(x1, y1, x2, y2) {
-  return { x: (x2 + x1) / 2, y: (y2 + y1) / 2 };
-}
+let reset = start(SIDES);
+//**
+//
+//
+//
+//
+//  */
 
-function drawInitialPoint(color = "black") {
-  const initialPoint = { x: width / 2, y: height / 2 };
-  drawPoint(initialPoint.x, initialPoint.y, color);
-  return initialPoint;
-}
-function drawPoint(x, y, color = "black") {
-  context.fillStyle = color;
-  context.beginPath();
-  context.arc(x, y, 1, 0, Math.PI * 2, true);
-  context.fill();
-  context.closePath();
+function sideChangeHandler(e) {
+  console.log(e.value);
+  reset();
+  SIDES = +e.value;
+  reset = start(SIDES);
 }
